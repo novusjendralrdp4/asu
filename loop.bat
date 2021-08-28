6b781f7a8b25f39a88fd421f7498003fa1db38ca
novusjendralrdp4 <89678344+novusjendralrdp4@users.noreply.github.com>
2021-08-28 14:50:27 +0700

    caca

--- loop.bat
+++ loop.bat
@@ -5,5 +5,5 @@ tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK t
 ping 127.0.0.1 > null
 cls
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
-timeout /t 30 /nobreak > NUL
+timeout /t 30 > NUL
 goto check
